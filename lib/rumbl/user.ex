6605b28d74d853c86a87f435dc06dfb31d6905d8
Deng Haolin <1020958553@qq.com>
2018-02-23 20:43:09 +0800

    first commit

---
+++ lib/rumbl/user.ex
@@ -0,0 +1,38 @@
+defmodule Rumbl.User do
+  alias RumblWeb
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :password_hash, :string
+    field :username, :string
+    field :password, :string, virtual: true
+
+    timestamps()
+  end
+
+  def changeset(user, params \\ %{}) do
+    user
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:name, min: 6, max: 20)
+    |> validate_length(:username, min: 6, max: 20)
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 30)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
+end
---
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -0,0 +1,49 @@
+defmodule RumblWeb.UserController do
+    use RumblWeb, :controller
+    alias Rumbl.Repo
+
+    def index(conn, _params) do
+        case authenticate(conn) do
+            %Plug.Conn{halted: ture} = conn ->
+                conn
+            conn ->
+                users = Repo.all(Rumbl.User)
+                render conn, "index.html", users: users
+        end
+    end
+
+    def show(conn, %{"id" => id}) do
+        user = Repo.get(Rumbl.User, id)
+        render conn, "show.html", user: user
+    end
+
+    alias Rumbl.User
+
+    def new(conn, _params) do
+        changeset = User.changeset(%User{})
+        render conn, "new.html", changeset: changeset
+    end
+
+    def create(conn, %{"user" => user_params}) do
+        changeset = User.registration_changeset(%User{}, user_params)
+        case Repo.insert(changeset) do
+            {:ok, user} ->
+                conn
+                |> put_flash(:info, "#{user.name} created!")
+                |> redirect(to: user_path(conn, :index))
+            {:error, changeset} ->
+                render(conn, "new.html", changeset: changeset)
+        end
+    end
+
+    def authenticate(conn) do
+        if conn.assign.current_user do
+            conn
+        else
+            conn
+            |> put_flash(:error, "you must be logged in!")
+            |> redirect(to: page_path(conn, :index))
+            |> halt()
+        end
+    end
+ end
