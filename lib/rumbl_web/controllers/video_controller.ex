a481e4d47a0e103d22af327bdd7084c0d230ef6f
Deng Haolin <1020958553@qq.com>
2018-03-02 16:15:20 +0800

    fix build_assoc

--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -1,6 +1,6 @@
 defmodule RumblWeb.VideoController do
   use RumblWeb, :controller
-
+  
   alias Rumbl.Repo
   alias Rumbl.Content
   alias Rumbl.Content.Video
@@ -11,8 +11,12 @@ defmodule RumblWeb.VideoController do
   end
 
   def new(conn, _params, user) do
-    changeset = Content.change_video(%Video{})
-    render(conn, "new.html", changeset: changeset)
+    # changeset = Content.change_video(%Video{})
+    changeset = 
+      user
+      |> Ecto.build_assoc(:videos)
+      |> Content.change_video(%Video{})
+      |> render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"video" => video_params}, user) do
