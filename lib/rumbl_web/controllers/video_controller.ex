e0883ee6f50a43dae39223b1dbe2153c559ee289
Deng Haolin <1020958553@qq.com>
2018-03-02 14:11:49 +0800

    revert new

--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -1,6 +1,6 @@
 defmodule RumblWeb.VideoController do
   use RumblWeb, :controller
-  
+
   alias Rumbl.Repo
   alias Rumbl.Content
   alias Rumbl.Content.Video
@@ -11,12 +11,8 @@ defmodule RumblWeb.VideoController do
   end
 
   def new(conn, _params, user) do
-    # changeset = Content.change_video(%Video{})
-    changeset = 
-      user
-      |> build_assoc(:videos)
-      |> Content.change_video(%Video{})
-      |> render(conn, "new.html", changeset: changeset)
+    changeset = Content.change_video(%Video{})
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"video" => video_params}, user) do
